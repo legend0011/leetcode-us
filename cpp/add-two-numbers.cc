12bf575f21e036f899ed473dce2b8bded9adcd56
张驰 <chzhang>
2017-06-08 21:03:50 +0800

    style fixed

--- cpp/add-two-numbers.cc
+++ cpp/add-two-numbers.cc
@@ -1,6 +1,5 @@
 // https://leetcode.com/problems/add-two-numbers/#/description
 // chi zhang, 2017-6-8
-
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -14,17 +13,17 @@ public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         ListNode* p1 = l1;
         ListNode* p2 = l2;
-        ListNode* dummy = new ListNode(0);
-        ListNode* p = dummy;
+        ListNode dummy(-1);
+        ListNode* p = &dummy;
         int promote = 0;
-        while (p1 || p2) {
+        while (p1 != nullptr || p2 != nullptr) {
             int val = Add(p1, p2, &promote);
-            if (p1 && p2) {
+            if (p1 != nullptr && p2 != nullptr) {
                 p1 = p1->next;
                 p2 = p2->next;
-            } else if (p1) {
+            } else if (p1 != nullptr) {
                 p1 = p1->next;
-            } else if (p2) {
+            } else if (p2 != nullptr) {
                 p2 = p2->next;
             }
             p->next = new ListNode(val);
@@ -34,21 +33,20 @@ public:
             p->next = new ListNode(promote);
             p = p->next;
         }
-        if (dummy->next) {
-            ListNode* rs = dummy->next;
-            delete dummy;
+        if (dummy.next != nullptr) {
+            ListNode* rs = dummy.next;
             return rs;
         }
-        delete dummy;
-        return NULL;
+        return nullptr;
+        // TODO(chzhang): Reuse p1, p2
     }
 private:
     int Add(ListNode* p1, ListNode* p2, int* promote) {
         int a1 = 0, a2 = 0;
-        if (p1) {
+        if (p1 != nullptr) {
             a1 = p1->val;
         }
-        if (p2) {
+        if (p2 != nullptr) {
             a2 = p2->val;
         }
         int sum = a1 + a2 + *promote;
